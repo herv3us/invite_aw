f4da192a009fc9d70e9c9583c0e1ee0cc95e901a
Sofia Hervéus <sofia@herveus.se>
2024-10-18 12:22:46 +0200

    small styling changes to card.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,54 +7,54 @@ function Card() {
     const timeText = "text-[20px] text-white"
     const titleText = "text-[25px] text-orange-400"
     const infoText = "text-[16px] text-white"
+    const textDiv = "flex gap-x-10 w-full mb-5"
+    const infoDiv = "w-[70%]"
     // #endregion
 
-    // region return
   return (
-    <div className='bg-black py-14 px-12 rounded text-white flex justify-center items-center flex-col'>
+    <div className='bg-black py-10 px-12 rounded text-white flex justify-center items-center flex-col'>
         <div className='w-[60%] mb-16'>
             <p className={header}>Happy</p>
             <p className={halloweenText}>HALLOWEEN</p>
-            <p className={header + ' text-red-800 text-[150px] '}>AW</p>
+            <p className={header + ' text-red-900 text-[140px] '}>AW</p>
         </div>
-        <div className='flex gap-x-16 w-full mb-4'>
+        <div className={textDiv}>
             <p className={timeText}>16.00-17.00</p>
-            <div>
-                <p className={titleText}>KONTORSHÄNG</p>
-                <p className={infoText}>Det bjuds på snacks och The Witches brew drink</p>
+            <div className={infoDiv}>
+                <p className={titleText}>THE WITCHES BREW</p>
+                <p className={infoText}>På kontoret bjuds det på snacks och en förhäxad drink</p>
             </div>
         </div>
-        <div className='flex gap-x-16 w-full mb-4'>
+            <div className={textDiv}>
             <p className={timeText}>17.00-17.30</p>
-            <div>
+            <div className={infoDiv}>
                 <p className={titleText}>TILL LISEBERG</p>
-                <p className={infoText}>Kollektivtrafik till kvällens huvudevent</p>
+                <p className={infoText}>Kollektivtrafik till kvällens huvudevent avgår kl 17.14 ifrån Järntorget</p>
             </div>
         </div>
-        <div className='flex gap-x-16 w-full mb-4'>
+            <div className={textDiv}>
             <p className={timeText}>17.30-19.15</p>
-            <div>
+            <div className={infoDiv}>
                 <p className={titleText}>6-KAMP</p>
                 <p className={infoText}>Vi tar oss in på Liseberg och ger oss in i en farlig strid</p>
             </div>
         </div>
-        <div className='flex gap-x-16 w-full mb-4'>
+            <div className={textDiv}>
             <p className={timeText}>19.30-21.20</p>
-            <div>
+            <div className={infoDiv}>
                 <p className={titleText}>KÄK PÅ GASTBAREN</p>
                 <p className={infoText}>Vi mättar magarna med rysliga burgare</p>
             </div>
         </div>
-        <div className='flex gap-x-16 w-full mb-4'>
+            <div className={textDiv}>
             <p className={timeText}>21.30-23.00</p>
-            <div>
+            <div className={infoDiv}>
                 <p className={titleText}>STRIDEN FORTSÄTTER</p>
-                <p className={infoText}>Kvällen är inte slut än - Vi fortsätter den svåra fighten om seger</p>
+                <p className={infoText}>Kvällen är inte slut än - Vem ska ta hem segern...</p>
             </div>
         </div>
     </div>
   )
-  // endregion
 }
 
 export default Card

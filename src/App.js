b6fa05aee60d4fea51e35955732829f63fc7786e
Sofia Hervéus <sofia@herveus.se>
2024-10-21 13:19:33 +0200

    add soundeffect to page and move out toggle to it's own file

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Card from "./components/Card";
+import sound from './assets/halloween.mp3'
+import Toggle from './components/Toggle';
 
 function App() {
-  const [language, setLanguage] = useState('sv'); // Default är svenska
+  const [language, setLanguage] = useState('sv');
+  const audioRef = useRef(null);
 
   const toggleLanguage = () => {
     setLanguage((prevLang) => (prevLang === 'sv' ? 'en' : 'sv'));
   };
 
+  const startSound = () => {
+    if (audioRef.current) {
+      audioRef.current.play().catch(error => {
+        console.error("Failed to play sound:", error);
+      });
+    }
+  };
+
   return (
-    <div className="bg-zinc-900 w-full h-screen flex justify-center items-center flex-col relative">
-       <div className="absolute top-5 right-5">
-        {/* Toggle switch */}
-        <div className="flex flex-col items-center">
-          <div
-            className={`w-10 h-6 sm:w-14 sm:h-8 flex items-center rounded-full p-1 cursor-pointer transition-colors duration-300 ${
-              language === 'sv' ? 'bg-gray-700' : 'bg-red-900'
-            }`}
-            onClick={toggleLanguage}
-          >
-            {/* Switch circle */}
-            <div
-              className={`h-4 w-4 sm:h-6 sm:w-6 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
-                language === 'sv' ? '' : 'translate-x-4 sm:translate-x-6'
-              }`}
-            />
-          </div>
-          {/* Labels under the toggle */}
-          <div className="flex gap-x-2 justify-between w-full mt-1 text-white text-[9px]">
-            <span className={`${language === 'sv' ? 'text-red-800' : ''}`}>Svenska</span>
-            <span className={`${language === 'en' ? 'text-red-800' : ''}`}>English</span>
-          </div>
-        </div>
-      </div>
+    <div className="bg-zinc-900 w-full h-screen flex justify-center items-center flex-col relative"
+    onClick={startSound}
+    >
+      <Toggle language={language} setLanguage={setLanguage} />
       <Card language={language} />
+      <audio ref={audioRef} src={sound} loop />
     </div>
   );
 }
---
+++ src/components/Toggle.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Toggle({language, setLanguage}) {
+    const toggleLanguage = () => {
+    setLanguage((prevLang) => (prevLang === 'sv' ? 'en' : 'sv'));
+    };
+
+  return (
+     <div className="absolute top-5 right-5">
+        {/* Toggle switch */}
+        <div className="flex flex-col items-center">
+          <div
+            className={`w-10 h-6 sm:w-14 sm:h-8 flex items-center rounded-full p-1 cursor-pointer transition-colors duration-300 ${
+              language === 'sv' ? 'bg-gray-700' : 'bg-red-900'
+            }`}
+            onClick={toggleLanguage}
+          >
+            {/* Switch circle */}
+            <div
+              className={`h-4 w-4 sm:h-6 sm:w-6 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
+                language === 'sv' ? '' : 'translate-x-4 sm:translate-x-6'
+              }`}
+            />
+          </div>
+          {/* Labels under the toggle */}
+          <div className="flex gap-x-2 justify-between w-full mt-1 text-white text-[9px]">
+            <span className={`${language === 'sv' ? 'text-red-800' : ''}`}>Svenska</span>
+            <span className={`${language === 'en' ? 'text-red-800' : ''}`}>English</span>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Toggle
